09ef606810075494a5fa05e6c31a068a9aef374b
Shane Stacy <shanestacy2@gmail.com>
2020-10-20 15:34:26 -0400

    more helpers

--- jvm/src/main/kotlin/SlimFramework/helpers.kt
+++ jvm/src/main/kotlin/SlimFramework/helpers.kt
@@ -39,3 +39,45 @@ fun frwToBytes(bytes: String): ByteArray {
 fun frwExceptionToString(e: Exception): String {
     return "Exception: ${e.message}"
 }
+
+fun frwGreaterThan(a: Int, b: Int): Boolean {
+    return a > b
+}
+
+fun frwLessThan(a: Int, b: Int): Boolean {
+    return a < b
+}
+
+fun <T>frwAreEqual(a: T, b: T): Boolean {
+    return a == b
+}
+
+fun <T1, T2>frwNotEqual(a: Int, b: Int): Boolean {
+    return !frwAreEqual(a, b)
+}
+
+fun <T>frwToSet(list: MutableList<T>): MutableSet<T> {
+    val set = mutableSetOf<T>()
+    for (item in list) {
+        set.add(item)
+    }
+    return set
+}
+
+fun frwComesBefore(a: Char, b: Char): Boolean {
+    return a < b
+}
+
+fun frwComesAfter(a: Char, b: Char): Boolean {
+    return a > b
+}
+
+fun frwGreaterThan(a: String, b: String): Boolean {
+    val result = a.compareTo(b)
+    return frwGreaterThan(result, 0)
+}
+
+fun frwLessThan(a: String, b: String): Boolean {
+    val result = a.compareTo(b)
+    return frwLessThan(result, 0);
+}
